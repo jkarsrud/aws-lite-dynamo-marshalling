7a4cc464e041eded80ee248f484f945c55d3ef78
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2024-03-06 23:45:50 +0100

    Use regular arc.tables methods and _client methods

--- index.test.mjs
+++ index.test.mjs
@@ -2,7 +2,6 @@ import { test } from "node:test";
 import assert from "node:assert";
 import sandbox from "@architect/sandbox";
 import arc from "@architect/functions";
-import { marshall } from "@aws-sdk/util-dynamodb";
 
 class Metadata {
   constructor(metadata) {
@@ -43,52 +42,26 @@ test("passing Model with metadata: new Metadata", async (ctx) => {
   const tables = await arc.tables();
   const tableName = tables.name("data");
 
-  await ctx.test("_doc", () => {
-    tables._doc.put(
-      {
-        TableName: tableName,
-        Item: event,
-      },
-      (err) => {
-        assert(
-          /convertClassInstanceToMap=true/.test(err),
-          "Throws error about class instances"
-        );
-      }
+  try {
+    await tables.data.put(event);
+  } catch (ex) {
+    console.log(ex);
+    assert(
+      /convertClassInstanceToMap=true/.test(ex),
+      "Throws error about converting class instance to map"
     );
-  });
+  }
 
-  await ctx.test("marshall without options throws", () => {
-    assert.throws(() => {
-      marshall(event);
-    }, /Pass options.convertClassInstanceToMap=true/);
-  });
-
-  await ctx.test("marshall with options does not throw", () => {
-    assert.doesNotThrow(() => {
-      marshall(event, {
-        convertClassInstanceToMap: true,
-      });
-    }, /Pass options.convertClassInstanceToMap=true/);
-  });
-
-  await ctx.test(
-    "putting marshalled object with options is a-okay",
-    (_, done) => {
-      tables._db.putItem(
-        {
-          TableName: tableName,
-          Item: marshall(event, {
-            convertClassInstanceToMap: true,
-          }),
-        },
-        async () => {
-          const { PK, SK } = event;
-          const item = await tables.data.get({ PK, SK });
-          console.log(item);
-          done();
-        }
-      );
-    }
-  );
+  try {
+    await tables._client.PutItem({
+      TableName: tableName,
+      Item: event,
+    });
+  } catch (ex) {
+    console.log(ex);
+    assert(
+      /convertClassInstanceToMap=true/.test(ex),
+      "Throws error about converting class instance to map"
+    );
+  }
 });
